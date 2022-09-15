958533bd81f7411c5ecd63ed47d24b9e1cecb7cb
Divjot-Babra <divjot.s.b@gmail.com>
2022-09-15 15:43:59 +1000

    Fixing issue with visp_auto_tracker package.

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/divjot/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/divjot/catkin_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/divjot/catkin_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/divjot/catkin_ws/src:$ROS_PACKAGE_PATH"
